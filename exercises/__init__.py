d9f29cd003240e5dc9246822374e5d1e8865815e
Nel Rivart <nel.rivart@ens.uvsq.fr>
2021-11-10 16:58:27 +0100

    TD4

---
+++ exercises/__init__.py
@@ -0,0 +1,6 @@
+import random
+l=[]
+for i in range (10000000000):
+    l.append(random.randint(9,99))
+print(l)
+    
--- test.py
+++ test.py
@@ -1,14 +1,33 @@
-
-def tempsEnSeconde(temps):
-    """ Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
-    jour=temps[0]*86400
-    heure=temps[1]*3600
-    minute=temps[2]*60
-    sec=temps[3]
-    tempsEnSeconde=jour+heure+minute+sec
-    return tempsEnSeconde
-
+def syracuse(n):
+    """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
+    l=[]
+    while n!=1:
+        if n%2==0:
+            n=n/2
+            l.append(n)
+            
+        else:
+            n=(n*3)+1
+            l.append(n)
+            
     
-temps = (3,23,1,34)
-print(type(temps))
-print(tempsEnSeconde(temps))  
+    else:
+        print(l)
+        print("fin")
+    return ""
+
+print(syracuse(int(input("entre un nombre: "))))
+
+def testeConjecture(n_max):
+    """ Teste la conjecture de Collatz pour toutes les valeurs de 2 à n_max """
+    
+    for n in range (2,(n_max)):
+        if syracuse(n)==1.0:
+            continue
+    return ""
+        
+            
+print(testeConjecture(10000))
+        
+            
+            
