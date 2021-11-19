1530bf2f1ca159a4cf52c797ee6736ce7c1de2ec
Nel Rivart <nel.rivart@ens.uvsq.fr>
2021-11-19 17:23:23 +0100

    TD5 avancement(2)

--- exercises/TD05_gui/dessin.py
+++ exercises/TD05_gui/dessin.py
@@ -25,7 +25,7 @@ def croix():
     x0 = r.randint(1,600)
     y0 = r.randint(1,600)
     canvas.create_line(y0, x0, y0-100 , x0-100,fill='yellow')
-    canvas.create_line(y0 -100, x0 +100, y0 , x0, fill="yellow")
+    canvas.create_line(y0 -100, x0, y0 , x0-100, fill="yellow")
 
     
     
