625b61382d22af997b900aec283408830e14ceac
Nel Rivart <nel.rivart@ens.uvsq.fr>
2021-11-22 20:52:29 +0100

    cible.py

--- exercises/TD05_gui/cible.py
+++ exercises/TD05_gui/cible.py
@@ -1,33 +1,33 @@
 import tkinter as tk
-
+global a,b
+a, b = 600, 600
 root = tk.Tk()
 root.title("Mon dessin")
 root.geometry("600x600")
-canvas = tk.Canvas(root, width = 600, height = 600,bg="black")
+canvas = tk.Canvas(root, width = a, height = b,bg="black")
+color=["blue", "green", "black", "yellow", "magenta", "red"]
+x0=1
+y0=1
+x1=b
+y1=a
+
+n=0
+for i in range (1,8):
+    if i==1:
+        n=0
+        print("i")
+    else:
+        n+=30
+    canvas.create_oval(y0+n, x0+n, y1-n , x1-n, outline=color[0],fill=color[0])
+    canvas.create_oval(y0+(5+n), x0+(5+n), y1-(5+n) , x1-(5+n), outline=color[1],fill=color[1])
+    canvas.create_oval(y0+(10+n), x0+(10+n), y1-(10+n) , x1-(10+n), outline=color[2],fill=color[2])
+    canvas.create_oval(y0+(15+n), x0+(15+n), y1-(15+n) , x1-(15+n), outline=color[3],fill=color[3])
+    canvas.create_oval(y0+(20+n), x0+(20+n), y1-(20+n) , x1-(20+n), outline=color[4],fill=color[4])
+    canvas.create_oval(y0+(25+n), x0+(25+n), y1-(25+n) , x1-(25+n), outline=color[5],fill=color[5])
 
 
-def cerclecolor():
-    
-    for i in range(1,300):
-        global couleur
-        couleur="blue"
-        x0 = i
-        y0 = i
-        if i % 1==0:
-            couleur="blue"
-        elif i % 2==0:
-            couleur="green"
-        elif i % 3==0:
-            couleur="black"
-        elif i % 4==0:
-            couleur="yellow"
-        elif i % 5==0:
-            couleur="magenta"
-        elif i % 6==0:
-            couleur="red"
-                       
-        canvas.create_oval(y0, x0, y0 +600 , x0 +600, outline=couleur,fill=couleur)
-        
+
+
 
 canvas.grid(row=2,column=1,rowspan=4,columnspan=4)
 root.mainloop()
