6ab9633c983b6554b46ab5c2d342ab7a662e7a3c
Nel Rivart <nel.rivart@ens.uvsq.fr>
2021-11-22 20:58:57 +0100

    cible.py fini

--- exercises/TD05_gui/cible.py
+++ exercises/TD05_gui/cible.py
@@ -12,18 +12,18 @@ x1=b
 y1=a
 n=0
 
-for i in range (1,10):
+for i in range (1,7):
     if i==1:
         n=0
         print("i")
     else:
-        n+=30
+        n+=54
     canvas.create_oval(y0+n, x0+n, y1-n , x1-n, outline=color[0],fill=color[0])
-    canvas.create_oval(y0+(5+n), x0+(5+n), y1-(5+n) , x1-(5+n), outline=color[1],fill=color[1])
-    canvas.create_oval(y0+(10+n), x0+(10+n), y1-(10+n) , x1-(10+n), outline=color[2],fill=color[2])
-    canvas.create_oval(y0+(15+n), x0+(15+n), y1-(15+n) , x1-(15+n), outline=color[3],fill=color[3])
-    canvas.create_oval(y0+(20+n), x0+(20+n), y1-(20+n) , x1-(20+n), outline=color[4],fill=color[4])
-    canvas.create_oval(y0+(25+n), x0+(25+n), y1-(25+n) , x1-(25+n), outline=color[5],fill=color[5])
+    canvas.create_oval(y0+(9+n), x0+(9+n), y1-(9+n) , x1-(9+n), outline=color[1],fill=color[1])
+    canvas.create_oval(y0+(18+n), x0+(18+n), y1-(18+n) , x1-(18+n), outline=color[2],fill=color[2])
+    canvas.create_oval(y0+(27+n), x0+(27+n), y1-(27+n) , x1-(27+n), outline=color[3],fill=color[3])
+    canvas.create_oval(y0+(36+n), x0+(36+n), y1-(36+n) , x1-(36+n), outline=color[4],fill=color[4])
+    canvas.create_oval(y0+(45+n), x0+(45+n), y1-(45+n) , x1-(45+n), outline=color[5],fill=color[5])
 
 
 
