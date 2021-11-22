3c8387e570296eecaf9ca0aeeaf147d38f0bd6b3
Nel Rivart <nel.rivart@ens.uvsq.fr>
2021-11-23 00:00:07 +0100

    td5

--- exercises/TD05_gui/couleurs.py
+++ exercises/TD05_gui/couleurs.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import random 
+
 global a,b
 a, b = 256, 256
 root = tk.Tk()
@@ -7,23 +9,30 @@ root.geometry("374x256")
 canvas = tk.Canvas(root, width = a, height = b,bg="black")
 
 
-def get_color(r, g, b):
+def get_color():
     """ Retourne une couleur à partir de ses composantes r, g, b entre 0 et 255"""
-    r=r.randint(0,250)
-    g=r.randint(0,250)
-    b=r.randint(0,250)
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    
     
     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
-color="white"
+
+
+
 
 def draw_pixel(i,j,color):
     canvas.create_rectangle( (i, j)*2,fill=color,outline="" )
+ 
+draw_pixel(256/2,256/2,get_color())  
+
+def ecran_aleatoire():
+    draw_pixel(100,100,get_color)
     
     
-draw_pixel(256/2,256/2,"white")  
     
 
-randomBTN = tk.Button(root, text ="Aléatoire", command = draw_pixel,fg="blue")
+randomBTN = tk.Button(root, text ="Aléatoire", command = ecran_aleatoire,fg="blue")
 randomBTN.grid(row=1,column=0)
 degradgrisBTN = tk.Button(root, text ="Dégradé gris", command = root.destroy,fg="blue")
 degradgrisBTN.grid(row=2,column=0)
