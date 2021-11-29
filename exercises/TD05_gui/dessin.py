c71b82ba864b07055bb88e09a7d1c26a3e36d361
Nel Rivart <nel.rivart@ens.uvsq.fr>
2021-11-29 15:37:57 +0100

    td5

--- exercises/TD05_gui/dessin.py
+++ exercises/TD05_gui/dessin.py
@@ -14,7 +14,13 @@ global objects
 objects=[]
 
 def undo():
-    ()
+    last=objects[-1]
+    objects.pop(last)
+    if last=="cercle":
+        canvas.delete(last)
+        
+    
+    
 
 def color():
     
@@ -26,13 +32,13 @@ def color():
 #création du cercle,carre et croix + coodonées 
                      
 def cercle():
-    x0 = r.randint(1,500)
-    y0 = r.randint(1,500)
+    x0 = r.randint(100,500)
+    y0 = r.randint(100,500)
     objects.append(canvas.create_oval(y0, x0, y0 -100 , x0 -100, outline=a,fill=a))
     
 def carre():
-    x0 = r.randint(1,500)
-    y0 = r.randint(1,500)
+    x0 = r.randint(100,500)
+    y0 = r.randint(100,500)
     objects.append(canvas.create_rectangle(y0, x0, y0 -100, x0 -100, outline=a,fill=a))
     
 def croix():
@@ -40,9 +46,10 @@ def croix():
     x1=x0+100
     y0=r.randint(1,500)
     y1=y0+100
-    canvas.create_line(x0+50,y0,x1-50,y1,width=10,fill=a,outline=a)
-    canvas.create_line(x0,y0+50,x1,y1-50,width=10,fill=a,outline=a)
+    objects.append(canvas.create_line(x0+50,y0,x1-50,y1,fill=a))
+    objects.append(canvas.create_line(x0,y0+50,x1,y1-50,fill=a))
    
+
 
 #création des boutons
 
@@ -58,7 +65,7 @@ croixBTN.grid(row=4,column=0)
 colorBTN = tk.Button(root, text ="Choisir une couleur", command = color,fg="red")
 colorBTN.grid(row=1,column=2)
 
-undoBTN = tk.Button(root, text="Undo", command = root.destroy,fg="red")
+undoBTN = tk.Button(root, text="Undo", command = undo,fg="red")
 undoBTN.grid(row=1,column=1)
 
 #création du canvas
