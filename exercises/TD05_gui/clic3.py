abd637f3d03994b50b88fd9326ad29eda2f31bc9
Nel Rivart <nel.rivart@ens.uvsq.fr>
2021-12-01 17:34:10 +0100

    clique

--- exercises/TD05_gui/clic3.py
+++ exercises/TD05_gui/clic3.py
@@ -4,17 +4,27 @@ root = tk.Tk()
 root.title("Mon dessin")
 root.geometry("500x500")
 canvas = tk.Canvas(root, width = 500, height = 500,bg="black")
+cord=[]
 
 def draw_pixel(x,y,color):
     canvas.create_rectangle( (x, y)*2,fill=color,outline="" )
-    
+
+
+        
     
 def clic(event):
     x, y = event.x, event.y
-    if (0<event.x<250) and (0<event.y<500):
-        canvas.create_oval(x +25, y +25, x -25, y-25 , outline="blue")
-    else:
-        canvas.create_oval(x +25, y +25, x -25, y-25 , outline="red")
+    cord.append(x,y)
+    
+    if len(cord)==4:
+        x=cord[1]
+        y=cord[2]
+        x1=cord[3]
+        y1=cord[4]
+        if (0<x<250) and (0<y<500) and (0<x1<250) and (0<y1<250):
+            canvas.create_line(x, y, x1, y1,outline="blue")
+        else:
+            canvas.create_oval(x +25, y +25, x -25, y-25 , outline="red")
         
     
 
